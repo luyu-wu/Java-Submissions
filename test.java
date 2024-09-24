c68c76576a2d29b2212707fbd6d4573efa482d92
luyu-wu <luyuwuvk@gmail.com>
2024-09-24 13:48:53 -0400

    test

--- test.java
+++ test.java
@@ -1,8 +1,9 @@
 public class test {
-    public static void main (String[] args) {
-		boolean test = true;
-		System.out.println(test);
-
-
-    }
+   public static void main(String[] args)    {
+       int total = 100;
+       int numPeople = 40;
+       double average;
+       average = total / (double) numPeople;
+       System.out.println("Average: " + average);
+   }
 }
