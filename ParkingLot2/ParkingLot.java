2588a8f0be098ff07a8de8ed1867a07d72b2e531
luyu-wu <luyuwuvk@gmail.com>
2025-02-25 14:29:23 -0500

    add

--- ParkingLot2/ParkingLot.java
+++ ParkingLot2/ParkingLot.java
@@ -6,11 +6,59 @@ public class ParkingLot {
         this.spots = spots;
     }
 
+    /**
+     * Finds the first available spot by iterating through the lot.
+     * Time complexity: O(n*m), where n is the number of rows and m is the number of columns.
+     * @return The first available spot or null if none is available.
+     */
     public ParkingSpot findFirstAvailableSpotMethod1() {
         for (int i = 0; i < spots.length; i++) {
             for (int j = 0; j < spots[i].length; j++) {
                 if (spots[i][j].isAvailable()) {
                     return spots[i][j];
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Finds the first available spot using a single loop.
+     * Time complexity is the same as the first one?
+     * @return The first available spot or null if none is available.
+     */
+    public ParkingSpot findFirstAvailableSpotMethod2() {
+        int total = spots.length * spots[0].length;
+
+        for (int i = 0; i < total; i++) {
+            int row = i / spots[0].length;
+            int col = i % spots[0].length;
+
+            if (spots[row][col].isAvailable()) {
+                return spots[row][col];
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Finds the first available spot assuming all occupied spots are grouped together.
+     * Has complexity of O(n + m) where n is the number of rows and m is the number of columns.
+     * @return The first available spot or null if none is available.
+     */
+    public ParkingSpot findFirstAvailableSpotMethod3() {
+        int first_col = -1;
+        for (int i = 0; i < spots.length; i++) {
+            if (spots[i][0].isAvailable()) {
+                first_col = i;
+                break;
+            }
+            first_col++;
+        }
+        if (first_col >= 0) {
+            for (int i = 0; i < spots[first_col].length; i++) {
+                if (spots[first_col][i].isAvailable()) {
+                    return spots[first_col][i];
                 }
             }
         }
--- ParkingLot2/program.java
+++ ParkingLot2/program.java
@@ -3,6 +3,9 @@ public class program {
     public static void main(String[] args) {
         ParkingSpot[][] spots = new ParkingSpot[5][5];
 
+        /***
+
+        */
         for (int i = 0; i < spots.length; i++) {
             for (int j = 0; j < spots[i].length; j++) {
                 spots[i][j] = new ParkingSpot(i, j, false);
@@ -11,6 +14,6 @@ public class program {
 
         ParkingLot lot = new ParkingLot(spots);
 
-        System.out.println(lot.findFirstAvailableSpotMethod1().toString());
+        System.out.println(lot.findFirstAvailableSpotMethod3().toString());
     }
 }
