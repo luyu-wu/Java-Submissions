03140deff58614e2b4a10ad7b582de1714461362
luyu-wu <luyuwuvk@gmail.com>
2025-05-23 12:24:46 -0400

    i dont think theres any changes??

--- Banking-App-SummativeCSA/loan.java
+++ Banking-App-SummativeCSA/loan.java
@@ -1,16 +1,25 @@
+import java.util.Date;
+
 public class loan {
 
     private double amount;
     private double interestRate;
+    private Date date;
 
     public loan(double amount, double interestRate) {
         this.amount = amount;
         this.interestRate = interestRate;
+        this.date = new Date();
     }
 
     public double getAmount() {
         return amount;
     }
 
-    public void pay(int account, int pay) {}
+    public void pay(account account, double pay) {
+        purchase payback = new purchase(-pay, account, account, "Loan Payment");
+
+        account.purchaseAPI(payback);
+        amount -= pay;
+    }
 }
