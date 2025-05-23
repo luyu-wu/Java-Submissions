aecaf90340882bdfcfc1f9ad3f935dadc0d3d5f4
luyu-wu <luyuwuvk@gmail.com>
2025-05-23 13:57:23 -0400

    anyhting more ?

--- Banking-App-SummativeCSA/main.java
+++ Banking-App-SummativeCSA/main.java
@@ -1,13 +1,9 @@
 public class main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        loan newLoan = new loan(1.00, 1.02);
         account student = new account(2012, "John");
         //System.out.println(student.readFile("Data/JohnLOANHISTORY.txt"));
-        // student.takeLoan(21039);
-        //System.out.println(student.readFile("Data/JohnLOANHISTORY.txt"));
-        student.saveLoan(newLoan);
+        student.takeLoan(21039);
         System.out.println(student.getBalance());
     }
 }
